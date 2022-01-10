295597f015baf5b356f522abc4e81286ffae802a
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2022-01-10 19:08:43 +0500

    Add files via upload

---
+++ BroadcastSystem/BroadcastSystem.cs
@@ -0,0 +1,67 @@
+﻿using MEC;
+using Qurre;
+using Qurre.API;
+using Qurre.Events;
+using Qurre.API.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Round = Qurre.API.Round;
+using Map = Qurre.API.Map;
+
+namespace BroadcastSystem
+{
+    public class BroadcastSystem : Plugin
+    {
+        public override string Developer => "KoToXleB#4663";
+        public override string Name => "BroadcastSystem";
+        public override Version Version => new Version(1, 0, 0);
+        public override int Priority => int.MinValue;
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public static Config CustomConfig { get; private set; }
+        public void RegisterEvents()
+        {
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Round.Start += RoundStarted;
+            Qurre.Events.Player.Join += PlayerJoin;
+        }
+        public void UnregisterEvents()
+        {
+            CustomConfigs.Remove(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Round.Start -= RoundStarted;
+            Qurre.Events.Player.Join -= PlayerJoin;
+        }
+        public static void RoundStarted()
+        {
+            CustomConfig.Reload();
+            Timing.CallDelayed(CustomConfig.BroadcastStartWaiting * 60f, () =>
+            {
+                Timing.RunCoroutine(BroadcastTiming(), "Broadcast");
+            });
+        }
+        public static IEnumerator<float> BroadcastTiming()
+        {
+            while (!Round.Ended || CustomConfig.BroadcastMessages.Count > 0)
+            {
+                int randomValue = UnityEngine.Random.Range(0, CustomConfig.BroadcastMessages.Count);
+                Map.Broadcast($"{CustomConfig.BroadcastMessages[randomValue]}", 10);
+                CustomConfig.BroadcastMessages.Remove(CustomConfig.BroadcastMessages[randomValue]);
+
+                yield return Timing.WaitForSeconds(CustomConfig.BroadcastMinutes * 60f);
+            }
+            yield break;
+        }
+        public static void PlayerJoin(JoinEvent ev)
+        {
+            ev.Player.Broadcast($"<color=yellow>{CustomConfig.Hello}</color> <color=red>{ev.Player.Nickname}</color> <color=yellow>{CustomConfig.Text}</color>\n<b>{Qurre.API.Server.Name}</b>", 7);
+        }
+    }
+}
---
+++ BroadcastSystem/Config.cs
@@ -0,0 +1,30 @@
+﻿using Qurre.API.Addons;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BroadcastSystem
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "BroadcastSystem";
+
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+        [Description("Сколько надо ждать минут после начала раунда? How many minutes should I wait after the start of the round?")]
+        public int BroadcastStartWaiting { get; set; } = 4;
+        [Description("Сколько надо ждать минут после очередного сообщения Broadcast? How many minutes do I have to wait after the next Broadcast message?")]
+        public int BroadcastMinutes { get; set; } = 4;
+        [Description("Сообщение Привет. Message Hello.")]
+        public string Hello { get; set; } = "Hello";
+        [Description("Сообщение Привет. Message Hello.")]
+        public string Text { get; set; } = "are you playing on the server";
+        [Description("Необходимо сюда написать сообщения Broadcast. It is necessary to write Broadcast messages here.")]
+        public List<string> BroadcastMessages { get; set; } = new List<string>()
+        {
+            "<color=yellow>Hello</color>",
+            "<color=yellow>How are you</color>",
+            "<color=yellow>Join in my Discord</color>"
+        };
+    }
+}
