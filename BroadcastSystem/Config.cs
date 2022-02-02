54a409df970ed77b3a09e1fd42e8a67edc86c435
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2022-02-02 20:00:56 +0500

    Update Config.cs

--- BroadcastSystem/Config.cs
+++ BroadcastSystem/Config.cs
@@ -1,4 +1,5 @@
-﻿using Qurre.API.Addons;
+using Qurre.API;
+using Qurre.API.Addons;
 using System.Collections.Generic;
 using System.ComponentModel;
 
@@ -11,15 +12,15 @@ namespace BroadcastSystem
 
         [Description("Enable the plugin?")]
         public bool IsEnable { get; set; } = true;
-        [Description("Сколько надо ждать минут после начала раунда? How many minutes should I wait after the start of the round?")]
+        [Description("How many minutes should I wait after the start of the round?")]
         public int BroadcastStartWaiting { get; set; } = 4;
-        [Description("Сколько надо ждать минут после очередного сообщения Broadcast? How many minutes do I have to wait after the next Broadcast message?")]
+        [Description("How many minutes do I have to wait after the next Broadcast message?")]
         public int BroadcastMinutes { get; set; } = 4;
-        [Description("Сообщение Привет. Message Hello.")]
-        public string Hello { get; set; } = "Hello";
-        [Description("Сообщение Привет. Message Hello.")]
-        public string Text { get; set; } = "are you playing on the server";
-        [Description("Необходимо сюда написать сообщения Broadcast. It is necessary to write Broadcast messages here.")]
+        [Description("How long in seconds will the message be displayed to the player?")]
+        public ushort BroadcastLongSeconds { get; set; } = 10;
+        [Description("Text for the player when he logged in to the server.")]
+        public string Text { get; set; } = $"<color=yellow>Hello <color=red>%player%</color> are you playing on the server</color>\n<b><color=yellow>My Server Name</color></b>";
+        [Description("It is necessary to write Broadcast messages here.")]
         public List<string> BroadcastMessages { get; set; } = new List<string>()
         {
             "<color=yellow>Hello</color>",
